698f61632bd1fd71cf43fd6d67739f5c9c68f468
john-quinones-2025 <john.quinones@utec.edu.pe>
2026-06-21 23:39:17 -0500

    cimientos00

---
+++ include/Estructuras.h
@@ -0,0 +1,37 @@
+#ifndef ESTRUCTURAS_H
+#define ESTRUCTURAS_H
+
+
+
+
+// estructura de la particula
+struct Particle {
+
+
+    int id;
+    double x, y;
+    double vx, vy;
+    double radius;
+};
+
+
+// estructura de la caja limitadora
+struct LimiteEspacial {
+
+
+
+    double centro_x;
+    double centro_y;
+    double mitad_ancho;
+    double mitad_alto;
+
+    // verficador si una particula se encuentra en los limites de la caja
+    bool contienePunto(double punto_x, double punto_y) const {
+        return (punto_x >= centro_x - mitad_ancho &&
+                punto_x <= centro_x + mitad_ancho &&
+                punto_y >= centro_y - mitad_alto &&
+                punto_y <= centro_y + mitad_alto);
+    }
+};
+
+#endif 
